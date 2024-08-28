eab00d9f0f78eed09bcd8854d8b5961284c32ce7
Martin Lago <martin.lago@houlak.paigo.com.uy>
2024-08-28 10:42:40 -0300

    Add main colors and splash screen

---
+++ MovieDB/MovieDB/Utilities/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  MovieDB
+//
+//  Created by Martin Lago on 28/8/24.
+//
+
+import SwiftUI
+
+// MARK: - Color definitions
+
+extension Color {
+    static let backgroundColor = Color("Background")
+    static let mainBlue = Color("Blue")
+    static let mainOrange = Color("Orange")
+}
